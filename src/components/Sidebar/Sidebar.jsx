c7aa2d4c5eaed515f9f1c03394467a00fbdfc27d
Jubair <jubair.ahmed7946@gmail.com>
2024-01-03 20:11:48 +0600

    Add: logo and dumpy data table added

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,9 @@
+import logo from "../../assets/images/logo-transparent.svg"
 const Sidebar = () => {
     return(
         <div>
-             <p> HELLO I Am Sidebar </p>
+            <img className="w-36 mx-auto rounded-lg px-2 my-6 bg-[#3131bc]" src={logo} alt="logo" />
+            
         </div>
     )}
 export default Sidebar;
