afe70e25f1b8c4a418e8470f360629dc8598aa0b
Jubair <jubair.ahmed7946@gmail.com>
2024-01-03 18:06:59 +0600

    Setup: overall

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Root";
+
+
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>
+    }
+])
+
+export default Router
