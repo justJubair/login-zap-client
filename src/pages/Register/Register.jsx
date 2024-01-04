c53a5ae2b86ac876216ef0fef55919bd9a8e55e6
Jubair <jubair.ahmed7946@gmail.com>
2024-01-04 23:58:33 +0600

    Add: register form data collected

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,26 +2,60 @@ import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo-transparent.svg";
 import { useState } from "react";
 const Register = () => {
-    const [inputValue, setInputValue] = useState("");
-    const [suggestions, setSuggestions] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  // State to store the checked checkboxes
+  const [checkedCheckboxes, setCheckedCheckboxes] = useState([])
 
-    // Dummy list of states
-    const stateNames = ["Gujarat", "Maharashtra", "Karnataka"]
+  // Handler function to toggle the checked state of a checkbox
+  const handleCheckboxChange = checkboxValue =>{
+      const isChecked = checkedCheckboxes.includes(checkboxValue)
 
-    const handleInputChange = e=>{
-       const value = e.target.value;
-       setInputValue(value)
+      if(isChecked){
+        // If the checkbox is already checked, remove it from the array
+        setCheckedCheckboxes(checkedCheckboxes.filter(item=> item !== checkboxValue))
+      } else {
+        // If the checkbox is not checked, add it to the array
+        setCheckedCheckboxes([...checkedCheckboxes, checkboxValue])
+      }
+  }
+
+  // Dummy list of states
+  const stateNames = ["Gujarat", "Maharashtra", "Karnataka"];
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
 
     //    Filter state names
-    const filteredSuggestions = stateNames.filter(state=> state.toLowerCase().includes(value.toLowerCase()))
+    const filteredSuggestions = stateNames.filter((state) =>
+      state.toLowerCase().includes(value.toLowerCase())
+    );
 
-    setSuggestions(filteredSuggestions)
-    }
+    setSuggestions(filteredSuggestions);
+  };
 
-    const handleSuggestionClick = (suggestion)=>{
-        setInputValue(suggestion)
-        setSuggestions([])
-    }
+  const handleSuggestionClick = (suggestion) => {
+    setInputValue(suggestion);
+    setSuggestions([]);
+  };
+
+  // Handler function to get value from register form
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const phone = form.phone.value;
+    const gender = form.gender.value;
+    const checkbox = checkedCheckboxes;
+    const city = form.city.value;
+    const state = inputValue;
+    const password = form.password.value;
+
+    console.table(name, email, phone, gender, checkbox, city, state, password)
+
+  };
 
   return (
     <div>
@@ -39,140 +73,141 @@ const Register = () => {
             <h1 className="text-5xl font-bold">Register now!</h1>
           </div>
           <div className="card shrink-0 w-full max-w-2xl shadow-2xl bg-base-100">
-            <form className="card-body">
-                {/* name and email */}
+            <form onSubmit={handleRegister} className="card-body">
+              {/* name and email */}
               <div className="flex items-center justify-between gap-4">
-              <div className="form-control w-full">
-                <label className="label">
-                  <span className="label-text">Name</span>
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="name"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control w-full">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  name="email"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
+                <div className="form-control w-full">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="name"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control w-full">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    name="email"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
               </div>
 
               {/* phone and gender */}
               <div className="flex items-center justify-between gap-4">
-              <div className="form-control w-full">
-                <label className="label">
-                  <span className="label-text">Phone</span>
-                </label>
-                <input
-                  type="number"
-                  name="phone"
-                  placeholder="phone number"
-                  className="input input-bordered [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-                  required
-                />
+                <div className="form-control w-full">
+                  <label className="label">
+                    <span className="label-text">Phone</span>
+                  </label>
+                  <input
+                    type="number"
+                    name="phone"
+                    placeholder="phone number"
+                    className="input input-bordered [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                    required
+                  />
+                </div>
+                <div className="form-control w-full">
+                  <label className="label">
+                    <span className="label-text">Gender</span>
+                  </label>
+                  <select
+                    defaultValue="male"
+                    name="gender"
+                    className="select select-bordered w-full"
+                  >
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="others">Others</option>
+                  </select>
+                </div>
               </div>
-              <div className="form-control w-full">
-                <label className="label">
-                  <span className="label-text">Gender</span>
-                </label>
-                <select defaultValue="male" name="gender" className="select select-bordered w-full">
-                  
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                  <option value="others">Others</option>
-                </select>
-              </div>
-              </div>
-             
-             {/* checkbox */}
+
+              {/* checkbox */}
               <div className="form-control">
                 <label className="label">
                   <span>How did you hear about us?</span>
                 </label>
 
-              <div className="flex justify-around items-center my-2">
-              <div className="flex items-center gap-2">
-              
-              <input
-                type="checkbox"
-                className="checkbox"
-                required
-              />
-               <span className="label-text">Linkedin</span>
-             </div>
-             <div className="flex items-center gap-2">
-              <input
-                type="checkbox"
-                className="checkbox"
-                required
-              />
-               <span className="label-text">Friends</span>
-             </div>
-             <div className="flex items-center gap-2">
-              <input
-                type="checkbox"
-                className="checkbox"
-                required
-              />
-               <span className="label-text">Job Portal</span>
-             </div>
-             <div className="flex items-center gap-2">
-              <input
-                type="checkbox"
-                className="checkbox"
-                required
-              />
-               <span className="label-text">Others</span>
-             </div>
-              </div>
-               
+                <div className="flex justify-around items-center my-2">
+                  <div className="flex items-center gap-2">
+                    <input type="checkbox" className="checkbox" value="Linkedin" checked={checkedCheckboxes.includes("Linkedin")}
+                    onChange={()=> handleCheckboxChange("Linkedin")}
+                    />
+                    <span className="label-text">Linkedin</span>
+                  </div>
+
+                  <div className="flex items-center gap-2">
+                    <input type="checkbox" className="checkbox" value="Friends" checked={checkedCheckboxes.includes("Friends")}
+                    onChange={()=> handleCheckboxChange("Friends")}/>
+                    <span className="label-text">Friends</span>
+                  </div>
+
+                  <div className="flex items-center gap-2">
+                    <input type="checkbox" className="checkbox" value="Job Portal" checked={checkedCheckboxes.includes("Job Portal")}
+                    onChange={()=> handleCheckboxChange("Job Portal")}/>
+                    <span className="label-text">Job Portal</span>
+                  </div>
+
+                  <div className="flex items-center gap-2">
+                    <input type="checkbox" className="checkbox" value="Others" checked={checkedCheckboxes.includes("Others")}
+                    onChange={()=> handleCheckboxChange("Others")}/>
+                    <span className="label-text">Others</span>
+                  </div>
+                </div>
               </div>
 
-              <div  className="flex items-center justify-between gap-4">
-              <div className="form-control w-full">
-                <label className="label">
-                  <span className="label-text">City</span>
-                </label>
-                <select defaultValue="mumbai" name="city" className="select select-bordered w-full">
-                  
-                  <option value="mumbai">Mumbai</option>
-                  <option value="pune">Pune</option>
-                  <option value="ahmedabad">Ahmedabad</option>
-                </select>
-              </div>
-              <div className="form-control w-full">
-                <label className="label">
-                  <span className="label-text">State</span>
-                </label>
-                <input
-                  type="text"
-                  value={inputValue}
-                  onChange={handleInputChange}
-                  placeholder="state"
-                  className="input input-bordered"
-                  required
-                />
-                {
-                    suggestions.length > 0 && <ul className=" list-none p-0 m-0">
-                        {suggestions.map((suggestion,index)=> <li className="cursor-pointer p-2 border-2 rounded-lg hover:bg-base-200" key={index} onClick={()=> handleSuggestionClick(suggestion)}>
-                            {suggestion}
-                        </li>)}
+              <div className="flex items-center justify-between gap-4">
+                <div className="form-control w-full">
+                  <label className="label">
+                    <span className="label-text">City</span>
+                  </label>
+                  <select
+                    defaultValue="mumbai"
+                    name="city"
+                    className="select select-bordered w-full"
+                  >
+                    <option value="mumbai">Mumbai</option>
+                    <option value="pune">Pune</option>
+                    <option value="ahmedabad">Ahmedabad</option>
+                  </select>
+                </div>
+                <div className="form-control w-full">
+                  <label className="label">
+                    <span className="label-text">State</span>
+                  </label>
+                  <input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    placeholder="state"
+                    className="input input-bordered"
+                    required
+                  />
+                  {suggestions.length > 0 && (
+                    <ul className=" list-none p-0 m-0">
+                      {suggestions.map((suggestion, index) => (
+                        <li
+                          className="cursor-pointer p-2 border-2 rounded-lg hover:bg-base-200"
+                          key={index}
+                          onClick={() => handleSuggestionClick(suggestion)}
+                        >
+                          {suggestion}
+                        </li>
+                      ))}
                     </ul>
-                }
+                  )}
+                </div>
               </div>
-              </div>
-
 
               <div className="form-control">
                 <label className="label">
