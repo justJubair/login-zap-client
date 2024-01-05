18077d0c8d75448a2ab45a733c267c5a61ca0e4e
Jubair <jubair.ahmed7946@gmail.com>
2024-01-05 19:50:21 +0600

    Add: user adding form added

--- src/components/AddUser/AddUser.jsx
+++ src/components/AddUser/AddUser.jsx
@@ -1,7 +1,62 @@
 const AddUser = () => {
-    return(
-        <div>
-             <p> HELLO I Am AddUser </p>
-        </div>
-    )}
+    const handleAddUser = e=>{
+        e.preventDefault()
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+        const role = form.role.value;
+        console.log(name, email, phone, role)
+    }
+
+  return (
+    <div className="max-w-screen-lg mx-auto">
+      {/* Heading */}
+      <h2 className="text-center font-bold text-2xl my-6">Add New User</h2>
+
+      {/* form */}
+      <div>
+        <form onSubmit={handleAddUser}>
+            {/* name and email */}
+         <div className="flex items-center gap-4">
+         <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            className="input input-bordered w-full"
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            className="input input-bordered w-full"
+            required
+          />
+         </div>
+
+         {/* phone and role */}
+          <div className="flex items-center gap-4 my-6">
+          <input
+            type="number"
+            placeholder="Phone number"
+            name="phone"
+            className="input input-bordered w-full [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+            required
+          />
+          <select name="role"  className="select select-bordered w-full">
+            <option disabled>
+             Role?
+            </option>
+            <option value="Mobile App Developer">Mobile App Developer</option>
+            <option value="Web Developer">Web Developer</option>
+            <option value="UI/UX Designer">UI/UX Designer</option>
+          </select>
+          </div>
+          <button className="btn btn-block btn-primary">Save user</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 export default AddUser;
