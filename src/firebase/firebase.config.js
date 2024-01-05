ca3589e928a82e1632ef450cc4e9dec7b371c5dd
Jubair <jubair.ahmed7946@gmail.com>
2024-01-06 00:57:29 +0600

    Update: .env file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCC9LB7KujGbzpKG0B4JCy4Ld8bObRGMWI",
-  authDomain: "login-zap-client.firebaseapp.com",
-  projectId: "login-zap-client",
-  storageBucket: "login-zap-client.appspot.com",
-  messagingSenderId: "713837666542",
-  appId: "1:713837666542:web:f5905d75f5cd54a37ed3ff"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
